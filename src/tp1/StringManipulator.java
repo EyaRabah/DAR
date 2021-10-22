51f314426f94715b5a3ba4f2a08d67755e43a3d5
EyaRabah <83927101+EyaRabah@users.noreply.github.com>
2021-10-22 23:12:54 +0100

    Update StringManipulator.java

--- src/tp1/StringManipulator.java
+++ src/tp1/StringManipulator.java
@@ -9,28 +9,64 @@ public class StringManipulator {
     }
 
     public void reverse(){
-        //do your stuff
-        System.out.println("reverse : ");
+    	    String s2 = "";
+	        for(int i = s.length() - 1; i >= 0; i--)
+	        {
+	            s2 = s2 + s.charAt(i);
+	        }
+        System.out.println("reverse : "+s2);
     }
 
     public void isPalindrome(){
-        //do your stuff
-        System.out.println("isPalindrome : ");
+        	boolean test=true ;
+	    	int i = 0;
+	    	int j= s.length() - 1;
+			 while (j > i) 
+			 {
+				 if (s.charAt(i) != s.charAt(j)) 
+				 {
+					test= false; 
+				 }
+				 i++;
+				 j--;
+			 }
+			 System.out.println("isPalindrome : " + test);
     }
 
     public void toUpperCase(){
-        //do your stuff
-        System.out.println("uppercase : ");
+       		String s2 = "";
+	        for(int i = 0; i < s.length(); i++) {
+	        	char c = s.charAt(i);
+	        	if ((int)c < 122 && (int)c > 97)  
+	        		s2= s2+ (char) (c - 32);
+	        	else 		
+	        		s2= s2+s.charAt(i);
+	        	}
+	        System.out.println("uppercase : "+ s.toUpperCase());
     }
 
     public void toLowerCase(){
-        //do your stuff
-        System.out.println("lowercase : ");
+        	 String s2 = "";
+	        for(int i = 0; i < s.length(); i++) {
+	        	char c = s.charAt(i);
+	        	if ((int)c < 91 && (int)c > 64) 
+	        		s2= s2+ (char) (c + 32);
+	        	else 
+	        		s2= s2+s.charAt(i);     
+	            }
+	        System.out.println("lowercase : "+ s2);
     }
 
     public void getVowelNumber(){
-        //do your stuff
-        System.out.println("vowelNumber : ");
+    	    int v=0;
+	        s = s.toLowerCase();
+	        for(int i = 0; i < s.length(); i++) {
+	           char c = s.charAt(i); 
+	           if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') { 
+	                v++;
+	           }
+	        }
+	        System.out.println("vowelNumber : " + v);
     }
 
     public static void main(String[] args) {
